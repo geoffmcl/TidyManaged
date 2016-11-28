50db797dfd94baf4600607d5d082f665111841c3
Geoff McLane <ubuntu@geoffair.info>
2016-11-28 19:09:54 +0100

    Add header to Test1/Program.cs

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿/* 
+ * Test1 Project - Program.cs - HTML Parser
+ * 
+ * (c) 2016 (W3C) MIT, ERCIM, Keio University
+ * See tidy.h for the copyright notice.
+ *
+ * Uses 'TidyManaged.dll`, loading 'tidy.dll' - Paths must 
+ * be set appropriately.
+ * 
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,9 +26,9 @@ namespace Test1
             // Note mistakes in html
             using (Document doc = Document.FromString("<html><title>test</tootle><body>asd</body>"))
             {
-                doc.ShowWarnings = false;
+                doc.ShowWarnings = false;   // these do not seem to work???
                 doc.Quiet = true;
-                // doc.OutputXhtml = true;
+                // doc.OutputXhtml = true; // does not seem to work?
                 doc.CleanAndRepair();
                 string parsed = doc.Save();
                 Console.WriteLine(parsed);
