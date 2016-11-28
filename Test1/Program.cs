a3326b9640a467d5d57fcf547ae0a5a9196914e8
Geoff McLane <ubuntu@geoffair.info>
2016-11-28 17:59:54 +0100

    Add a 'Test1' projects

---
+++ Test1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TidyManaged;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test1 app. started...");
+            // Note mistakes in html
+            using (Document doc = Document.FromString("<html><title>test</tootle><body>asd</body>"))
+            {
+                doc.ShowWarnings = false;
+                doc.Quiet = true;
+                // doc.OutputXhtml = true;
+                doc.CleanAndRepair();
+                string parsed = doc.Save();
+                Console.WriteLine(parsed);
+            }
+            Console.WriteLine("Test1 app. ending...");
+        }
+    }
+}
